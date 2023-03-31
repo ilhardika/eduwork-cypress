8ba9f21a9b6a64898200f09d432ec7b0715d4404
ilhardika <ilhardika67@gmail.com>
2023-03-31 17:56:22 +0700

    working on saucelabs

---
+++ Cypress Fundamental/cypress/e2e/0-personal-test/swag_labs.cy.js
@@ -0,0 +1,10 @@
+/// <reference types = "cypress" /> 
+
+describe('swag labs testing', () => {
+    before(() => {
+        cy.visit('https://www.saucedemo.com/')
+      })
+    it('Standard user testing', () => {
+        
+    });
+});
