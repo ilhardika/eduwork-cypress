7449e6e54adecaa343f6ec44a9d8c510a57197fe
ilhardika <ilhardika67@gmail.com>
2023-04-02 11:27:28 +0700

    working on zero webb app test

---
+++ Cypress Fundamental/cypress/e2e/0-personal-test/zerowebapp.cy.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress" />
+
+describe("zero webb app pay bills testing", () => {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/bank/pay-bills.html')
+
+        
+    })
+})
