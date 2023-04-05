014a40254a2239fb8ff996c89b8727cd71a25a44
ilhardika <ilhardika67@gmail.com>
2023-04-05 19:55:27 +0700

    finish suacelabs testing

--- Cypress Advance/cypress/e2e/0-personal-project/saucelabs.cy.js
+++ Cypress Advance/cypress/e2e/0-personal-project/saucelabs.cy.js
@@ -6,12 +6,12 @@ describe('swag labs testing', () => {
     });
     
     it('Standard user testing', () => {
-      cy.fixture("user").then(user =>{
-        const standard = user.standard
-        const password = user.password
-        const namaDepan = user.namaDepan
-        const namaBelakang = user.namaBelakang
-        const kodePos = user.kodePos
+      cy.fixture("user_saucelabs").then(user_saucelabs =>{
+        const standard = user_saucelabs.standard
+        const password = user_saucelabs.password
+        const namaDepan = user_saucelabs.namaDepan
+        const namaBelakang = user_saucelabs.namaBelakang
+        const kodePos = user_saucelabs.kodePos
         
         //login
         cy.get('[data-test="username"]').type(standard)
@@ -20,9 +20,9 @@ describe('swag labs testing', () => {
         //inventory url assertions
         cy.url().should('include', '/inventory.html')
 
-        //when button click
+        //when add to cart button click
         cy.get('#add-to-cart-sauce-labs-backpack').click()
-        //button assertions
+        //remove button assertions
         cy.get('.pricebar').children('button').should('contain.text', 'Remove')
         //shoping cart asssertions
         cy.get('.shopping_cart_link').children('.shopping_cart_badge').should('be.visible')
@@ -60,9 +60,9 @@ describe('swag labs testing', () => {
     });
 
     it('Logout user testing', () => {
-      cy.fixture("user").then(user =>{
-        const logout = user.logout
-        const password = user.password
+      cy.fixture("user_saucelabs").then(user_saucelabs =>{
+        const logout = user_saucelabs.logout
+        const password = user_saucelabs.password
         
         //login
         cy.get('[data-test="username"]').type(logout)
@@ -72,4 +72,99 @@ describe('swag labs testing', () => {
         cy.get('[data-test="error"]').should('contain.text', 'Sorry, this user has been locked out.')
       });
     });
+
+    it("Problem user testing", () => {
+      cy.fixture("user_saucelabs").then(user_saucelabs => {
+        const problem = user_saucelabs.problem
+        const password = user_saucelabs.password
+        const namaDepan = user_saucelabs.namaDepan
+        const namaBelakang = user_saucelabs.namaBelakang
+        const kodePos = user_saucelabs.kodePos
+
+        //login
+        cy.get('[data-test="username"]').type(problem)
+        cy.get('[data-test="password"]').type(password)
+        cy.get('[data-test="login-button"]').click()
+
+        //when add to cart button click
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
+        //when add to cart button click again
+        cy.get('#remove-sauce-labs-backpack').click()
+        //remove button assertions
+        cy.get('#remove-sauce-labs-backpack').should('contain.text', 'Remove').and('not.contain.text', 'Add to cart')
+
+        //cart testing
+        cy.get('#shopping_cart_container').click()
+        //cart url assertions
+        cy.url().should('include', '/cart.html')
+
+        //checkout step one testing
+        cy.get('#checkout').click()
+        //checkout step one url assertions
+        cy.url().should('include', '/checkout-step-one.html')
+
+        //fill form
+        cy.get('#first-name').type(namaDepan)
+        cy.get('#last-name').type(namaBelakang)
+        cy.get('#postal-code').type(kodePos)
+        cy.get('#continue').click()
+
+        //cannot fill last name form, error assertions
+        cy.get('[data-test="error"]').should('contain.text', 'Error: Last Name is required')
+      })
+    });
+
+    it('Glitch user testing', () => {
+      cy.fixture('user_saucelabs').then(user_saucelabs => {
+        const glitch = user_saucelabs.glitch
+        const password = user_saucelabs.password
+        const namaDepan = user_saucelabs.namaDepan
+        const namaBelakang = user_saucelabs.namaBelakang
+        const kodePos = user_saucelabs.kodePos
+
+        //login
+        cy.get('[data-test="username"]').type(glitch)
+        cy.get('[data-test="password"]').type(password)
+        cy.get('[data-test="login-button"]').click()
+        //inventory url assertions
+        cy.url().should('include', '/inventory.html')
+
+        //when add to cart button click
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
+        //remove button assertions
+        cy.get('.pricebar').children('button').should('contain.text', 'Remove')
+        //shoping cart asssertions
+        cy.get('.shopping_cart_link').children('.shopping_cart_badge').should('be.visible')
+
+        //cart testing
+        cy.get('#shopping_cart_container').click()
+        //cart url assertions
+        cy.url().should('include', '/cart.html')
+
+        //checkout step one testing
+        cy.get('#checkout').click()
+        //checkout step one url assertions
+        cy.url().should('include', '/checkout-step-one.html')
+
+        //fill form
+        cy.get('#first-name').type(namaDepan)
+        cy.get('#last-name').type(namaBelakang)
+        cy.get('#postal-code').type(kodePos)
+
+        //checkout step two testing
+        cy.get('#continue').click()
+        //checkout step two url assertions
+        cy.url().should('include','/checkout-step-two.html')
+
+        //checkout complete testing
+        cy.get('#finish').click()
+        //checkout complete url assertions
+        cy.url().should('include', '/checkout-complete.html')
+
+        //back to product button
+        cy.get('#back-to-products').click()
+        //back to product assertons
+        cy.url().should('include', '/inventory.html')
+      })
+    })
 });
