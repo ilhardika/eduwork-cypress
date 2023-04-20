a2bafae7fd58bfaa240238aec9801a893df8c388
ilhardika <ilhardika67@gmail.com>
2023-04-20 10:46:53 +0700

    adding .prettierrc

--- BDD/cypress.config.js
+++ BDD/cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  video: false,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 10000, e2e: {
+    specPattern: "**/*.feature",
+    supportFile: false,
+    setupNodeEvents,
   },
 });
