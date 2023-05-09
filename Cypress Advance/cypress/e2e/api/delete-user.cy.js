2d531a2c858d37e251ac94d0b31b621198475e46
ilhardika <ilhardika67@gmail.com>
2023-05-09 16:20:30 +0700

    adding api delete method

---
+++ Cypress Advance/cypress/e2e/api/delete-user.cy.js
@@ -0,0 +1,7 @@
+desribe('delete user', () => {
+    it.only('Delete user', () => {
+        cy.request('DELETE', 'https:reqres.in/api/users/2').then((response) => {
+            expect(response.status).equal(204)
+        })
+    })
+})
