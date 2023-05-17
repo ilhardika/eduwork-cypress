cf1d2e443ba228f4d33070ce79f5473fe7dd63fe
ilhardika <ilhardika67@gmail.com>
2023-05-17 12:52:22 +0700

    adding test in headers-body.cy.js

--- Cypress Advance/cypress/e2e/api/pokeapi/headers-body.cy.js
+++ Cypress Advance/cypress/e2e/api/pokeapi/headers-body.cy.js
@@ -1,14 +1,25 @@
-it.only('Validate Header', () => {
+it('Validate Header', () => {
     cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
     // assert headers
     cy.get('@pokemon').its('headers').its('content-type')
     .should('include', 'application/json; charset=utf-8')
     
+    // assert response body
+    cy.get('@pokemon').its('body').should('include', {name: "ditto"})
     // assert response body menggunakan expect
     cy.get('@pokemon').then((response) => {
-        expect(response.body).to.have.property('name', 'ditto')
+        expect(response.body.abilities[0].ability.name).to.eq("limber")
+        expect(response.body.abilities[0].ability.url).to.eq('https://pokeapi.co/api/v2/ability/7/');
     })
 
     //assert status
     cy.get('@pokemon').its('status').should('equal', 200)
+});
+it('Validate Negative Response', () => {
+    cy.request({
+        method: 'GET',
+        url: 'https://pokeapi.co/api/v2/pokemon/eduwork',
+        failOnStatusCode: false
+    }).as('pokemon')
+    cy.get('@pokemon').its('status').should('equal', 404)
 });
