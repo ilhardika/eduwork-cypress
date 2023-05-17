e7b1aa2bf2c5a12487e9a98734c1a9e4247e0a0b
ilhardika <ilhardika67@gmail.com>
2023-05-17 11:40:48 +0700

    adding status code validation

--- Cypress Advance/cypress/e2e/api/pokeapi/headers-body.cy.js
+++ Cypress Advance/cypress/e2e/api/pokeapi/headers-body.cy.js
@@ -1,9 +1,14 @@
 it.only('Validate Header', () => {
     cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+    // assert headers
     cy.get('@pokemon').its('headers').its('content-type')
     .should('include', 'application/json; charset=utf-8')
+    
     // assert response body menggunakan expect
     cy.get('@pokemon').then((response) => {
         expect(response.body).to.have.property('name', 'ditto')
     })
+
+    //assert status
+    cy.get('@pokemon').its('status').should('equal', 200)
 });
